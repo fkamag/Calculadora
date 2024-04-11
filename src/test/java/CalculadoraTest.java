3a09fd392e9c81cc9536b8826aa0f381895f5f9b
fkamag <fabiokamagian@gmail.com>
2024-04-11 16:15:35 -0300

    feat DisplayName

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,16 +1,20 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Testes da classe Calculadora")
 public class CalculadoraTest {
 
   @Test
+  @DisplayName("Teste do método somar")
   void testeSomar() {
     Calculadora calculadora = new Calculadora();
     assertEquals(34, calculadora.somar(32, 2));
   }
 
   @Test
+  @DisplayName("Teste do método subtrair")
   void testeSubtrair() {
     Calculadora calculadora = new Calculadora();
     assertEquals(1, calculadora.subtrair(3, 2));
